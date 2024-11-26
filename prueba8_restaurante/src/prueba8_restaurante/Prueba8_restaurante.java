0638279ab2440cfa9b7946d46fd7bc581a2d924d
Chung-Un <chungunyum23@gmail.com>
2024-11-26 11:09:11 -0600

    Cambios

--- prueba8_restaurante/src/prueba8_restaurante/Prueba8_restaurante.java
+++ prueba8_restaurante/src/prueba8_restaurante/Prueba8_restaurante.java
@@ -4,7 +4,9 @@
  */
 package prueba8_restaurante;
 
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  *
@@ -14,6 +16,7 @@ import java.util.Scanner;
 public class Prueba8_restaurante {
 
     public static cliente cliente= new cliente();
+    public static menu menu = new menu();
     /**
      * @param args the command line arguments
     
@@ -22,20 +25,22 @@ public class Prueba8_restaurante {
     
  
     public static void validarMenu(){
-    int dia=0;
-    switch(dia){
-        case 1:
+    
     
     
     
     }
+    public static double descuentos(double total){
+    double montoDescuentos=0;
     
     
-    
+        
+        
+    return montoDescuentos;
     }
     
     public static void main(String[] args) {
-        
+        String usuarioInput;
         int dia;
         
         Scanner entrada = new Scanner (System.in);
@@ -53,6 +58,29 @@ public class Prueba8_restaurante {
         System.out.println("Ingrese el dia actual: \n1.Lunes\n2.Martes\n3.Miercoles\n4.Jueves\n5.Viernes\n6.Sabado\n7.Domingo");
         dia = entrada.nextInt();
         entrada.nextLine();
+        
+        System.out.println("Es cliente frecuente (s/n)");
+        usuarioInput = entrada.next().toLowerCase();
+        
+        if(usuarioInput.equals("s")){
+        cliente.setFrecuente(true);
+                }
+        else{
+        cliente.setFrecuente(false);
+        
+        }
+        
+        System.out.println("Es mayor de edad?(s/n)");
+        usuarioInput = entrada.next().toLowerCase();
+        
+        if(usuarioInput.equals("s")){
+        cliente.setMayorEdad(true);   
+        }
+        else{
+        cliente.setMayorEdad(false);
+        
+        }
+        
         
         
         
--- prueba8valor/src/prueba8valor/Prueba8valor.java
+++ prueba8valor/src/prueba8valor/Prueba8valor.java
@@ -90,4 +90,4 @@ public class Prueba8valor {
         
     }
     
-}
+
